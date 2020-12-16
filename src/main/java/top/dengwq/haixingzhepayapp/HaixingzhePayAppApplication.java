291e8e6dcf0b8ebd0d2390b954cea179fde420f9
dengweiqiang <dengweiqiang66@163.com>
2020-12-17 00:12:42 +0800

    Springboot 项目初始化

---
+++ src/main/java/top/dengwq/haixingzhepayapp/HaixingzhePayAppApplication.java
@@ -0,0 +1,13 @@
+package top.dengwq.haixingzhepayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaixingzhePayAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaixingzhePayAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/dengwq/haixingzhepayapp/HaixingzhePayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package top.dengwq.haixingzhepayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaixingzhePayAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
